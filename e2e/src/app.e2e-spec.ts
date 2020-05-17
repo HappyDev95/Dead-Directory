a4f5127887eaab2586ef8b875fbc9c1569fdeeca
HappyDev95 <Sam_Hapgood@yahoo.com>
2020-05-17 19:14:27 -0400

    Dist file modification

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('WhosRunning app is running!');
+    expect(page.getTitleText()).toEqual('Dead Directory app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/WhosRunning'),
+      dir: require('path').join(__dirname, './coverage/Dead Directory'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
